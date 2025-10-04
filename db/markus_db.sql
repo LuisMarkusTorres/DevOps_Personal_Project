c818eaa91b28cce1ee1df307b5478a051dace07d
LuisMarkusTorres <luismarkus31@gmail.com>
2025-10-04 14:17:05 +0000

    updated table constraints

--- db/markus_db.sql
+++ db/markus_db.sql
@@ -4,7 +4,7 @@ USE MarkusDB;
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(128) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
