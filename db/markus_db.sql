18d8a6ba5de63be6e6cccc7e7420556d4f18ff40
LuisMarkusTorres <luismarkus31@gmail.com>
2025-10-04 14:16:11 +0000

    updated form validation

--- db/markus_db.sql
+++ db/markus_db.sql
@@ -4,7 +4,7 @@ USE MarkusDB;
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(25) NOT NULL,
-  `password` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
